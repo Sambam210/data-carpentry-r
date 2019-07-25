2dfe90cc0be3750c3896eed990e3f6f67f17da10
Samiya Tabassum <samiya.tabassum@mq.edu.au>
2019-07-25 11:25:08 +1000

    Add scripts to dl data, ignore data folder

---
+++ scripts/Git_Setup.R
@@ -0,0 +1,9 @@
+# Git set up
+
+# this wasn;t working on console
+system('git config --global user.email "samiya.tabassum@mq.edu.au"')
+system('git config --global user.name "Sambam210"')
+
+# typed everything in the '' into the terminal, changed user name
+system('git config --global user.email "samiya.tabassum@mq.edu.au"')
+system('git config --global user.name "Samiya Tabassum"')
