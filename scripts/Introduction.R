53b434b36437b270c6491e9e91b2e2bd72a4efe3
Samiya Tabassum <samiya.tabassum@mq.edu.au>
2019-07-25 15:57:31 +1000

    functions

--- scripts/Introduction.R
+++ scripts/Introduction.R
@@ -9,3 +9,15 @@ area_acres <- 2.47 * area_hectares
 length <- c(1,2,3)
 width <- c(4,5,6)
 area <- length * width
+
+# functions
+
+# b <- sqrt(a)
+
+round(3.14159)
+# change the number of decimal places
+round(3.14159, digits = 2)
+
+
+
+
